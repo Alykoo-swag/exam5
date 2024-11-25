0fbc840de529514f3c8065c90bba7b50cea70a50
AlyKoo <alykooabilhan@gmail.com>
2024-11-25 17:29:51 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+// let num1 = +prompt('San engiz:');
+// let num2 = +prompt('San engiz:');
+// let tanba = prompt('Tanba engiz:');
+
+// const calculator = (a, b, c) => {
+//     if (c === '+') {
+//         return a + b;
+//     } else if (c === '-') {
+//         return a - b;
+//     } else if (c === '*') {
+//         return a * b;
+//     } else if (c === '/') {
+//         if (b === 0) {
+//             return;
+//         }
+//         return a / b;
+//     } else {
+//         return;
+//     }
+// };
+// console.log(calculator(num1, num2, tanba)); 
+
+
+// 2-tapsyrma
+
+
+// function jenildikter() {
+
+//     let jenildik = 0;
+
+//     if (barlihBaga > 20000) {
+//         jenildik = 0.30; 
+//     } else if (barlihBaga > 10000) {
+//         jenildik = 0.20; 
+//     } else if (barlihBaga > 5000) {
+//         jenildik = 0.10;
+//     } else { 
+//         return;
+//     }
+// };
+
+
+//3-shi bonus tapsyrma
+
+
+// let a = 15;
+// let b = 20;
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+// alert(a); // 20
+// alert(b); // 15
